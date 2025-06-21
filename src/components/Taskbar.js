0dddfb0b0e6f7ff4f4c8c90a2f65f2285e0305ce
Márton Orova <orova.marton@gmail.com>
2025-06-21 14:17:18 +0200

    Add logo in taskbar and change favicon

--- src/components/Taskbar.js
+++ src/components/Taskbar.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { Navbar, Container, Button } from 'react-bootstrap';
+import tekeresLogo from '../small-logo.png';
 
 function Taskbar({ onSolutionButtonClick }) {
   return (
     <Navbar bg="dark" variant="dark" fixed="top">
       <Container>
-        <Navbar.Brand>Tekeres Agency Nyilvántartás</Navbar.Brand>
+        <Navbar.Brand>
+          <img
+            src={tekeresLogo}
+            alt="Icon"
+            style={{ width: '25px', marginRight: '10px' }}
+          />
+          Tekeres Agency Nyilvántartás
+        </Navbar.Brand>
         <Button onClick={onSolutionButtonClick}>Rendszer Frissítés</Button>
       </Container>
     </Navbar>
